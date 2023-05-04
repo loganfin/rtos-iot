67d3d3f99f086e28e6f712387fa07d4d8dc6ae17
Logan Finley <finley4.logan@gmail.com>
2023-05-03 20:03:44 -0700

    Send visible light value to the IOT server

--- src/iot_server.cpp
+++ src/iot_server.cpp
@@ -1,5 +1,6 @@
 #include "iot_server.h"
 #include "auth.h"
+#include "light_sensor.h"
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
@@ -17,26 +18,39 @@ void vIotServer(void* parameters)
     DynamicJsonDocument json_buffer(2048);
     String http_request_data;
 
-    json_buffer["auth_code"] = AUTH_CODE;
-    serializeJson(json_buffer, http_request_data);
+    uint16_t vis_light;
 
     while (true) {
-            if((xTaskGetTickCount() - last_time) > delay) {
-                http.useHTTP10(true);
-                http.begin(client, SERVER_ADDR);
-                http.addHeader("Content-Type", "application/json");
+        if((xTaskGetTickCount() - last_time) > delay) {
+            http.useHTTP10(true);
+            http.begin(client, SERVER_ADDR);
+            http.addHeader("Content-Type", "application/json");
 
-                int http_response_code = http.POST(http_request_data);
+            xQueueReceive(xQVisibleLight, &vis_light, 0);
 
-                deserializeJson(json_buffer, http.getStream());
-                Serial.print("Server address: ");
-                Serial.println(SERVER_ADDR);
-                Serial.print("Request json: ");
-                Serial.println(http_request_data);
-                Serial.print("Response json: ");
-                Serial.println(json_buffer.as<String>());
+            json_buffer["auth_code"] = AUTH_CODE;
+            json_buffer["temperature"] = 0.0;
+            json_buffer["humidity"] = vis_light;
+            json_buffer["light"] = 0.0;
+            json_buffer["latitude"] = 0.0;
+            json_buffer["longitude"] = 0.0;
+            json_buffer["altitude"] = 0.0;
+            json_buffer["time"] = "2023-05-04 00:00:01";
+            serializeJson(json_buffer, http_request_data);
 
-                last_time = xTaskGetTickCount();
-            }
+            int http_response_code = http.POST(http_request_data);
+
+            deserializeJson(json_buffer, http.getStream());
+            Serial.print("Server address: ");
+            Serial.println(SERVER_ADDR);
+            Serial.print("Request json: ");
+            Serial.println(http_request_data);
+            Serial.print("Response json: ");
+            Serial.println(json_buffer.as<String>());
+
+            last_time = xTaskGetTickCount();
+
+            http_request_data = "";
+        }
     }
 }
